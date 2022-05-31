82c5a8dee32e462db4661cf0708791244856ae6f
hakandindis <hakandindis@gmail.com>
2022-05-31 17:36:32 +0300

    profile_photo changed with real photo, info text modified, history_card's layout optimized

--- lib/feature/profile/view/profile_view.dart
+++ lib/feature/profile/view/profile_view.dart
@@ -2,6 +2,8 @@
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:coffee_app/feature/profile/widget/order_list.dart';
+import 'package:coffee_app/product/padding/horizontal_padding.dart';
+import 'package:coffee_app/product/padding/page_padding.dart';
 import 'package:flutter/material.dart';
 
 class ProfileView extends StatelessWidget {
@@ -13,25 +15,53 @@ class ProfileView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Row(
-          children: [
-            CircleAvatar(
-              minRadius: 50,
-              backgroundImage: AssetImage("assets/coffee/coffee4.jpg"),
-            ),
-            Column(
-              children: [
-                Text("HAKAN DINDIŞ"),
-              ],
-            ),
-          ],
-        ),
-        Text(
-          "Order History",
-          style: Theme.of(context).textTheme.headline4?.copyWith(color: Colors.orange),
-        ),
+        UserSection(),
+        OrderText(),
         OrderList(stream: stream),
       ],
+    );
+  }
+}
+
+class OrderText extends StatelessWidget {
+  const OrderText({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      "Order History",
+      style: Theme.of(context).textTheme.headline4?.copyWith(color: Colors.orange),
+    );
+  }
+}
+
+class UserSection extends StatelessWidget {
+  const UserSection({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const PagePadding.all(),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          CircleAvatar(
+            minRadius: 50,
+            backgroundImage: AssetImage("assets/profile/profile_photo.jpg"),
+          ),
+          Padding(
+            padding: HorizontalPadding.symmetric(),
+            child: Text(
+              "HAKAN DINDIŞ",
+              style: Theme.of(context).textTheme.headline5?.copyWith(fontWeight: FontWeight.bold),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/feature/profile/widget/order_card.dart
+++ lib/feature/profile/widget/order_card.dart
@@ -1,3 +1,5 @@
+import 'package:coffee_app/product/padding/horizontal_padding.dart';
+import 'package:coffee_app/product/padding/page_padding.dart';
 import 'package:coffee_app/product/utility/order_model_enum.dart';
 import 'package:flutter/material.dart';
 
@@ -13,31 +15,40 @@ class OrderCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Expanded(
-          flex: 7,
-          child: Row(
-            children: List.generate(
-              drinkNames.length,
-              (index) => Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  "${drinkNames[index]}",
-                  style: Theme.of(context).textTheme.subtitle1,
+    return Padding(
+      padding: const PagePadding.all(),
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        decoration: BoxDecoration(color: Colors.grey[800], borderRadius: BorderRadius.circular(30)),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+              flex: 7,
+              child: Wrap(
+                children: List.generate(
+                  drinkNames.length,
+                  (index) => Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      "${drinkNames[index]}",
+                      style: Theme.of(context).textTheme.subtitle1,
+                    ),
+                  ),
                 ),
               ),
             ),
-          ),
+            Expanded(
+              flex: 3,
+              child: Text(
+                "\$${data[OrderProperties.totalPrice.name]}",
+                style: const TextStyle(color: Colors.orange, fontWeight: FontWeight.bold, fontSize: 25),
+              ),
+            ),
+          ],
         ),
-        Expanded(
-          flex: 3,
-          child: Text(
-            "\$${data[OrderProperties.totalPrice.name]}",
-            style: const TextStyle(color: Colors.orange, fontWeight: FontWeight.bold, fontSize: 25),
-          ),
-        ),
-      ],
+      ),
     );
   }
 }
